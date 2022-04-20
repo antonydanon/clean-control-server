fd5dc5716e56f2d4582d75e51350ca2439d87650
KaShTaN27 <dkachan.lida@mail.ru>
2022-04-20 20:58:09 +0300

    Create getAllWorkersFromOrder & getWorkerLocation endpoints

--- src/main/java/com/bsuir/clean_control_server/service/WorkerService.java
+++ src/main/java/com/bsuir/clean_control_server/service/WorkerService.java
@@ -1,20 +1,39 @@
 package com.bsuir.clean_control_server.service;
 
-import com.bsuir.clean_control_server.dto.WorkerLocation;
+import com.bsuir.clean_control_server.dto.SendLocationDTO;
+import com.bsuir.clean_control_server.dto.ReceiveLocationDTO;
+import com.bsuir.clean_control_server.exception.ResourceNotFoundException;
+import com.bsuir.clean_control_server.model.Worker;
 import com.bsuir.clean_control_server.repository.WorkerRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 @Service
+@Transactional
 @RequiredArgsConstructor
 public class WorkerService {
     private final WorkerRepository workerRepository;
+    private final OrderService orderService;
 
-    @Transactional
-    public WorkerLocation newLocation(WorkerLocation workerLocation){
-        workerRepository.updateLocation(workerLocation.getLatitude(), workerLocation.getLongitude(), workerLocation.getPhoneNumber());
-        return workerLocation;
+    public Worker getWorkerById(Long workerId) {
+        return workerRepository.findById(workerId).orElseThrow(() ->
+                new ResourceNotFoundException("Worker with id: " + workerId + " doesn't exists in database"));
     }
 
+    public ReceiveLocationDTO newLocation(ReceiveLocationDTO receiveLocationDTO){
+        workerRepository.updateLocation(receiveLocationDTO.getLatitude(), receiveLocationDTO.getLongitude(), receiveLocationDTO.getPhoneNumber());
+        return receiveLocationDTO;
+    }
+
+    public List<Worker> getAllWorkersByOrderId(Long orderId) {
+        return workerRepository.findAllByOrder(orderService.getOrderById(orderId));
+    }
+
+    public SendLocationDTO getWorkerLocation(Long workerId) {
+        Worker worker = getWorkerById(workerId);
+        return new SendLocationDTO(worker.getLatitude(), worker.getLongitude());
+    }
 }
