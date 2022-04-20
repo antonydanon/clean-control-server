19a3afa57eb639503edd5a8d09f501eb554f8e9d
KaShTaN27 <dkachan.lida@mail.ru>
2022-04-20 20:55:56 +0300

    Create method that finds manager by phone

--- src/main/java/com/bsuir/clean_control_server/service/ManagerService.java
+++ src/main/java/com/bsuir/clean_control_server/service/ManagerService.java
@@ -1,5 +1,7 @@
 package com.bsuir.clean_control_server.service;
 
+import com.bsuir.clean_control_server.exception.ResourceNotFoundException;
+import com.bsuir.clean_control_server.model.Manager;
 import com.bsuir.clean_control_server.repository.ManagerRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -7,6 +9,11 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class ManagerService {
+
     private final ManagerRepository managerRepository;
 
+    public Manager getManagerByPhone(String phone) {
+        return managerRepository.getManagerByPhoneNumber(phone).orElseThrow(() ->
+           new ResourceNotFoundException("Manager with phone: " + phone + " doesn't exists in database"));
+    }
 }
