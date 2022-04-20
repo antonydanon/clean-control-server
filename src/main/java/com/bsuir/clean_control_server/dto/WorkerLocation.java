fbff8d9122d7584007702078f17ea883946de940
Anton <antondanik32@mail.ru>
2022-04-20 17:29:29 +0300

    Create WorkerLocation dto

---
+++ src/main/java/com/bsuir/clean_control_server/dto/WorkerLocation.java
@@ -0,0 +1,14 @@
+package com.bsuir.clean_control_server.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+@AllArgsConstructor
+public class WorkerLocation {
+    private String phoneNumber;
+    private double longitude;
+    private double latitude;
+}
