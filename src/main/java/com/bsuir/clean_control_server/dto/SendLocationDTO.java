fbb88fb4ee13e3cf67c6d9e54c2915d7a2afd207
KaShTaN27 <dkachan.lida@mail.ru>
2022-04-20 20:51:11 +0300

    Create SendLocationDTO

---
+++ src/main/java/com/bsuir/clean_control_server/dto/SendLocationDTO.java
@@ -0,0 +1,11 @@
+package com.bsuir.clean_control_server.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class SendLocationDTO {
+    private double latitude;
+    private double longitude;
+}
