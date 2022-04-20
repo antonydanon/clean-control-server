0f8210ea7eb3533c4e053aeb2a5fb9ca0e6e7393
KaShTaN27 <dkachan.lida@mail.ru>
2022-04-20 20:54:59 +0300

    Add getManagerByPhoneNumber method into Repository

--- src/main/java/com/bsuir/clean_control_server/repository/ManagerRepository.java
+++ src/main/java/com/bsuir/clean_control_server/repository/ManagerRepository.java
@@ -1,10 +1,12 @@
 package com.bsuir.clean_control_server.repository;
 
 import com.bsuir.clean_control_server.model.Manager;
-import com.bsuir.clean_control_server.model.Worker;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ManagerRepository extends JpaRepository<Manager, Long> {
+    Optional<Manager> getManagerByPhoneNumber(String phone);
 }
