fed943dafaa1b39038bcbf084c02d92dae1bc7aa
Anton <antondanik32@mail.ru>
2022-04-20 16:22:59 +0300

    Initial commit

---
+++ src/main/java/com/bsuir/clean_control_server/CleanControlServerApplication.java
@@ -0,0 +1,13 @@
+package com.bsuir.clean_control_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CleanControlServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CleanControlServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bsuir/clean_control_server/CleanControlServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bsuir.clean_control_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CleanControlServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
