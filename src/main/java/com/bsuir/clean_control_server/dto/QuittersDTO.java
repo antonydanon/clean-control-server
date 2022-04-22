74294fd8b80a5ae5f6e3e5866a82076ac0b6a6e3
KaShTaN27 <dkachan.lida@mail.ru>
2022-04-22 22:48:10 +0300

    Create QuittersDTO

---
+++ src/main/java/com/bsuir/clean_control_server/dto/QuittersDTO.java
@@ -0,0 +1,14 @@
+package com.bsuir.clean_control_server.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+@Setter
+public class QuittersDTO {
+    private String name;
+    private String surname;
+    private String phone;
+}
